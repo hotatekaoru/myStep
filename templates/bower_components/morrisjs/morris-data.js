b2cffd938945d9d1d15d97061bd67078c1527ff3
Hotate <kaorupochi@gmail.com>
2016-08-18 12:21:12 +0900

    add Index page mock

---
+++ templates/bower_components/morrisjs/morris-data.js
@@ -0,0 +1,30 @@
+$(function() {
+
+    Morris.Bar({
+        element: 'morris-bar-chart',
+        data: [{
+            y: 'Coding',
+            a: 100,
+            b: 90
+        }, {
+            y: 'Training',
+            a: 75,
+            b: 65
+        }, {
+            y: 'Housework',
+            a: 50,
+            b: 40
+        }, {
+            y: 'Aggregate',
+            a: 120,
+            b: 10
+        }],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Kaoru', 'Yuri'],
+        hideHover: 'auto',
+        resize: true,
+        barColors: ['rgb(31,92,183)','rgb(91,184,121)'],
+    });
+    
+});
