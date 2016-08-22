1eba25f489fb76089a3b7dd5bc9ac6f760c2d1ad
Hotate <kaorupochi@gmail.com>
2016-08-22 23:43:06 +0900

    refactor html/css

--- model/user.go
+++ model/user.go
@@ -29,7 +29,7 @@ var db = database.GetDB()
 func Auth(form *validation.S01Form) (int, error) {
 	user := User{}
 
-	db.Debug().Where(&User{UserName: form.UserName}).Find(&user)
+	db.Debug().Model(&User{}).Where(&User{UserName: form.UserName}).Find(&user)
 	if (User{}) == user { return 0, errors.New(constant.MSG_ENABLE_LOGIN) }
 
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(form.Password))
